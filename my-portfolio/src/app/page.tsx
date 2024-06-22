e1fd317fea265a7db18ed3d193e56aedd1abbbc5
Natasha Heng <natashahengjy@gmail.com>
2024-06-22 16:19:12 +0800

    Template is out

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -1,21 +1,16 @@
-import Image from "next/image";
+import SideBar from "@/sidebar/sidebar";
 
-export default function Home() {
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          This is the landing page
+    <main className="flex p-24">
+      <SideBar />
+      <div className="w-full items-center font-mono text-sm lg:flex border w-1/4 p-2">
+        <p className="">
+          where accordion will be
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          </a>
-        </div>
+      </div>
+      <div className="w-full items-center font-mono text-sm lg:flex border p-2">
+          main content
       </div>
     </main>
   );
---
+++ my-portfolio/src/sidebar/sidebar.tsx
@@ -0,0 +1,12 @@
+export default function SideBar() {
+    return(
+    <div className="w-full items-center font-mono text-sm lg:flex border w-1/4 p-2">
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+        </ul>
+    </div>
+    );
+}
